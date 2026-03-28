3dc05e60f1efb3c1da0db0950eda39e55963f13d
banna12345bob <banna12345bob@outlook.com>
2026-03-28 15:00:46 +1000

    Render drawn map in cartography table

---
+++ src/main/java/com/idiotss/maps/mixin/CartographyTableScreenMixin.java
@@ -0,0 +1,109 @@
+package com.idiotss.maps.mixin;
+
+import com.idiotss.maps.AllDataComponents;
+import com.idiotss.maps.AllItems;
+import com.idiotss.maps.MapDrawingClient;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.CartographyTableScreen;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.MapItem;
+import net.minecraft.world.level.saveddata.maps.MapId;
+import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+@Mixin(CartographyTableScreen.class)
+public class CartographyTableScreenMixin {
+    @Shadow
+    @Final
+    private static ResourceLocation BG_LOCATION;
+
+    @Shadow
+    @Final
+    private static ResourceLocation ERROR_SPRITE;
+
+    @Shadow
+    @Final
+    private static ResourceLocation DUPLICATED_MAP_SPRITE;
+
+    @Shadow
+    @Final
+    private static ResourceLocation MAP_SPRITE;
+
+    @Inject(method = "renderBg",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V"),
+            cancellable = true)
+    private void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY, CallbackInfo ci) {
+        int i = ((AbstractContainerScreen) (Object) this).getGuiLeft();
+        int j = ((AbstractContainerScreen) (Object) this).getGuiTop();
+        guiGraphics.blit(BG_LOCATION, i, j, 0, 0, ((AbstractContainerScreen) (Object) this).getXSize(), ((AbstractContainerScreen) (Object) this).getYSize());
+        ItemStack itemstack = ((AbstractContainerScreen) (Object) this).getMenu().getSlot(1).getItem();
+        boolean flag = itemstack.is(Items.MAP);
+        boolean flag1 = itemstack.is(Items.PAPER);
+        boolean flag2 = itemstack.is(Items.GLASS_PANE);
+        ItemStack itemstack1 = ((AbstractContainerScreen) (Object) this).getMenu().getSlot(0).getItem();
+        if (!itemstack1.is(AllItems.DRAWABLE_MAP)) {
+            return;
+        }
+        MapId mapid = itemstack1.get(DataComponents.MAP_ID);
+        boolean flag3 = false;
+        List<Integer> pixels;
+        if (mapid != null) {
+            pixels = itemstack1.get(AllDataComponents.MAP_PIXELS);
+        } else {
+            pixels = null;
+        }
+
+        renderResultingMap(guiGraphics, mapid, pixels, flag, flag1, flag2, flag3);
+        ci.cancel();
+    }
+
+    private void renderResultingMap(
+            GuiGraphics guiGraphics,
+            @Nullable MapId mapId,
+            @Nullable List<Integer> pixels,
+            boolean hasMap,
+            boolean hasPaper,
+            boolean hasGlassPane,
+            boolean isMaxSize
+    ) {
+        int i = ((AbstractContainerScreen) (Object) this).getGuiLeft();
+        int j = ((AbstractContainerScreen) (Object) this).getGuiTop();
+        if (hasMap) {
+            guiGraphics.blitSprite(DUPLICATED_MAP_SPRITE, i + 67 + 16, j + 13, 50, 66);
+            renderMap(guiGraphics, mapId, pixels, i + 86, j + 16, 0.34F);
+            guiGraphics.pose().pushPose();
+            guiGraphics.pose().translate(0.0F, 0.0F, 1.0F);
+            guiGraphics.blitSprite(DUPLICATED_MAP_SPRITE, i + 67, j + 13 + 16, 50, 66);
+            renderMap(guiGraphics, mapId, pixels, i + 70, j + 32, 0.34F);
+            guiGraphics.pose().popPose();
+        } else {
+            guiGraphics.blitSprite(MAP_SPRITE, i + 67, j + 13, 66, 66);
+            renderMap(guiGraphics, mapId, pixels, i + 71, j + 17, 0.45F);
+        }
+    }
+
+    public void renderMap(GuiGraphics guiGraphics, @Nullable MapId mapId, @Nullable List<Integer> pixels, int x, int y, float scale)
+    {
+        if (mapId != null && pixels != null) {
+            guiGraphics.pose().pushPose();
+            guiGraphics.pose().translate((float)x, (float)y, 1.0F);
+            guiGraphics.pose().scale(scale, scale, 1.0F);
+            MapDrawingClient.getInstance().getMapRenderer().render(guiGraphics.pose(), guiGraphics.bufferSource(), mapId, pixels, true, 15728880);
+            guiGraphics.flush();
+            guiGraphics.pose().popPose();
+        }
+    }
+
+}
